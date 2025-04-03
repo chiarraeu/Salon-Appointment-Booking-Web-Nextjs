0e9472229dc848226453a47b1dbd7c300abe2fcc
Chiarra <103839751+chiarraeu@users.noreply.github.com>
2025-04-03 11:32:30 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#0D7DFF",
+          DEFAULT: "#d1716b",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
